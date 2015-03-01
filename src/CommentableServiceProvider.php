93b5ae989f38f0b793f91bff8794b3bde4f14b9b
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-03-02 01:55:42 +0700

    Add service provider

---
+++ src/CommentableServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Namest\Commentable;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class CommentableServiceProvider
+ *
+ * @author  Nam Hoang Luu <nam@mbearvn.com>
+ * @package Namest\Commentable
+ *
+ */
+class CommentableServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot up resources
+     */
+    public function boot()
+    {
+        // Publish a config file
+        $this->publishes([
+            __DIR__ . '/../config/likeable.php' => config_path('likeable.php')
+        ], 'config');
+
+        // Publish your migrations
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => base_path('/database/migrations')
+        ], 'migrations');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
